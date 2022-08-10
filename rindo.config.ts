838e4b637578db2390ace03f7ac1f97920e8d30b
NKDuy <kn145660@gmail.com>
2022-08-10 20:48:51 +0700

    first commit

---
+++ rindo.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@rindo/core';
+
+export const config: Config = {
+  namespace: 'rindo-starter-project-name',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
